7f1232977640de4399142780800f775b956cb59a
Pradipto Dutta <110712955+Pradipta7171@users.noreply.github.com>
2024-09-09 22:55:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
   },
 };
 
